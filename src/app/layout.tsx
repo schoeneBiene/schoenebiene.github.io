5647b03e8065f8ef182492ad26cf631164fe7459
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-04-22 16:15:15 +0200

    Clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<div className="bg-gradient-to-tr from-gray-900 to-gray-600">
+					{children}
+				</div>
+			</body>
 		</html>
 	);
 }
