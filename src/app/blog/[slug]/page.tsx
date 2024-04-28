368e03626c73d735f7a628f9bf138f1123138fa8
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-04-28 17:36:37 +0200

    Add blog pages in routing

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage({ params }: { params: { slug: string } }) {
+    return(
+        <div>
+            In contruction!
+        </div>
+    )
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogHome() {
+    return(
+        <div>
+            In construction!
+        </div>
+    )
+}
