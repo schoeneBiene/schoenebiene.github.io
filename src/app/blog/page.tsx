a57d9706a37beb7bb9294f9dd639356d45a41b32
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-04 17:17:23 +0200

    Make text white

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ const BlogList = lazy(() => import("@/components/BlogList"))
 export default function BlogHome() {
     return(
         <div className="w-full h-screen">
-            <div className="text-center">
+            <div className="text-center text-white text-xl">
                 <Suspense fallback="Loading...">
                     <BlogList />
                 </Suspense>
