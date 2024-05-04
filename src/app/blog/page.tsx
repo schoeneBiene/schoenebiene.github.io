5a2f22bce862014b156a6b8dec2777c134824f21
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-04 17:00:36 +0200

    Center blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,10 +4,12 @@ const BlogList = lazy(() => import("@/components/BlogList"))
 
 export default function BlogHome() {
     return(
-        <div>
-           <Suspense fallback="Loading...">
-            <BlogList />
-           </Suspense>
+        <div className="w-full h-screen">
+            <div className="text-center">
+                <Suspense fallback="Loading...">
+                    <BlogList />
+                </Suspense>
+           </div>
         </div>
     )
 }
