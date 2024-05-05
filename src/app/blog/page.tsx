18b0776f399e6805f9e1a3b3462e10478f8df056
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-05 11:20:36 +0200

    Add header to bloglist

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,6 +5,7 @@ const BlogList = lazy(() => import("@/components/BlogList"))
 export default function BlogHome() {
     return(
         <div className="w-full h-screen">
+            <h1 className="text-center text-white text-5xl p-4 underline">Blog Posts</h1>
             <div className="text-center text-white text-xl">
                 <Suspense fallback="Loading...">
                     <BlogList />
