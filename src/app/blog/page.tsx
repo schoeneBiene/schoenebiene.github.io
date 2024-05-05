65dba761900d25bd9b37cf2e689be9fd372b5aad
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-05 11:31:09 +0200

    Add go back to blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,23 @@
-import {Suspense, lazy} from "react"
+import Link from "next/link";
+import { Suspense, lazy } from "react";
 
-const BlogList = lazy(() => import("@/components/BlogList"))
+const BlogList = lazy(() => import("@/components/BlogList"));
 
 export default function BlogHome() {
-    return(
+    return (
         <div className="w-full h-screen">
-            <h1 className="text-center text-white text-5xl p-4 underline">Blog Posts</h1>
+            <div className="flex items-center justify-center p-4 relative">
+                <Link href="/" className="absolute left-4 top-1/2 -translate-y-1/2 text-white underline text-lg">
+                    Go back
+                </Link>
+                <h1 className="text-center text-white text-5xl p-4 underline">Blog Posts</h1>
+            </div>
             <div className="text-center text-white text-xl">
                 <Suspense fallback="Loading...">
                     <BlogList />
                 </Suspense>
-           </div>
+            </div>
         </div>
-    )
+    );
 }
+
