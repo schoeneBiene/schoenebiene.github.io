f42e3f42f61a9f815174d78682bc38e2c4a85705
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-06 19:12:26 +0200

    add title to blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,10 @@ import {Suspense, lazy} from "react"
 
 const BlogPost = lazy(() => import("@/components/BlogPost"))
 
+export const metadata = {
+    title: "goodbee's blog"
+}
+
 export default function BlogPage({ params }: { params: { slug: string } }) {
     return(
         <div className="text-white text-center">
