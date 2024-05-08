0b2aa04cc5e974c72d60e29dc8163a3b9a3d9f69
goodbee <100511746+schoeneBiene@users.noreply.github.com>
2024-05-08 20:35:09 +0200

    Add RegEx replacements to blog posts

--- src/components/BlogPostContent.tsx
+++ src/components/BlogPostContent.tsx
@@ -1,9 +1,20 @@
 import Markdown from 'react-markdown';
 
-export default function BlogPostContent({ content }: { content: string }) {
+const REGEX_REPLACEMENTS: Map<RegExp, string> = new Map([
+    [/gh:(.*)[/](.*)[#]([1-9]*)/g, "[$1/$2#$3](https://github.com/$1/$2/issues/$3)"], // link to github issues/pr's
+    [/(^|\s)(\/?([ru])\/(\w+))/g, "$1[$2](<https://reddit.com/$3/$4>)"] // link to reddit users or subreddits
+]);
+
+export default function BlogPostContent({ content = "" }: { content: string }) {
+    var replacedContent = content;
+
+    REGEX_REPLACEMENTS.forEach((value, key) => {
+        replacedContent = replacedContent.replace(key, value);
+    });
+
     return(
         <Markdown className="text-lg text-center mx-auto overflow-hidden break-words">
-            {content}
+            {replacedContent}
         </Markdown>
     );
 }
